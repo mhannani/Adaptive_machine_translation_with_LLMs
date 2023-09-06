a2ceed23c2c4e290e334b0dbce3f6a025a066b1a
mhannani <mohamed.hanani2019@gmail.com>
2023-09-06 07:37:10 +0100

    Wrapping up gpt translation

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import os
 import json
+from dotenv import load_dotenv
 from src.selectors.fuzzy import Fuzzy
 from src.helpers.get import parse_toml
+from src.prompt.gpt import Prompt
+from src.translators.chatgpt import GPT
 
 
 if __name__ == "__main__":
     # toml path
     toml_path: str = "./config/config.toml"
+    
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # OpenAI API key
+    openai_api_key: str = os.getenv("OPENAI_API_KEY")
     
     # parsing toml
     config = parse_toml(toml_path)
@@ -28,4 +37,14 @@ if __name__ == "__main__":
     # k fuzzy matches
     k_fuzzy_matches = fuzzy.get_top_k(sentence = source_sentence, k = 5)
 
-    print(k_fuzzy_matches)
+    # creating prompt object
+    prompt = Prompt(source_sentence, k_fuzzy_matches)
+
+    # gpt prompt
+    gpt_prompt = prompt()
+    
+    # gpt instance
+    gpt: GPT = GPT(openai_api_key)
+
+    output = gpt.translate(gpt_prompt)
+    print("output: ", output) # لا يمكننا التنبؤ به ولا يمكننا التحكم فيه.
---
+++ src/prompt/gpt.py
@@ -0,0 +1,72 @@
+from typing import List
+
+
+class Prompt:
+    """
+    LLM Prompt construction class
+    """
+
+    def __init__(self, source_sentence: str, fuzzy_matches: List) -> None:
+        """
+        Prompt class constructor
+
+        :param source_sentence str
+            The source sentence to translate
+        
+        :param fuzzy_matches List
+            List of fuzzy matches
+
+        :return None
+        """
+        
+        # source_sentence
+        self.source_sentence: str = source_sentence
+
+        # fuzzy_match
+        self.fuzzy_matches: List = fuzzy_matches
+    
+    def __call__(self, source_language: str = "English", target_language: str = "Arabic") -> str:
+        """
+        Construct a promp given sentence, and fuzzy matches
+
+        :param source_language str
+            The source language
+        
+        :param target_language str
+            The target langauge
+
+        :return str
+        """
+
+        # Initialize the prompt list with a system message
+        prompt = [{"role": "system", "content": f"Act like a good translator from {source_language} to {target_language}"}]
+
+        # Iterate over fuzzy matches and add user and assistant messages
+        for match in self.fuzzy_matches:
+            # source sentence
+            source_sentence = match["source_sentence"]
+
+            # target sentence
+            target_sentence = match["target_sentence"]
+
+            # user message
+            user_message = {"role": "user", "content": f"English: {source_sentence}"}
+            
+            # assistant message
+            assistant_message = {"role": "assistant", "content": f"Arabic: {target_sentence}"}
+
+            # append current user message
+            prompt.append(user_message)
+
+            # append current assistant message
+            prompt.append(assistant_message)
+
+        # Finally, add the user's original sentence to the prompt
+        user_message = {"role": "user", "content": f"English: {self.source_sentence}"}
+
+        # append the query sentence
+        prompt.append(user_message)
+
+        # return the constructed prompt
+        return prompt
+
